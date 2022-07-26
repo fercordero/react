1a1ab1154422976729c331a1ce4cfd47056db465
Fernanda Cordero <fernandacorderoparedes@gmail.com>
2022-07-26 12:57:59 -0500

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    return(
+        <ul> 
+        <li>HOME</li> 
+        <li>MUJER</li> 
+        <li>HOMBRE</li> 
+        <li>REBAJAS</li> 
+        <li>FAQs</li> 
+      </ul> 
+    )
+}
+
+export default NavBar;
